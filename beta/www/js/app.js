8989eb96d58c7fc6a6fcf1fc494aff2084743504
juliancoleman <julcol03@gmail.com>
2015-07-08 10:20:19 -0700

    we have working routes and a side menu! MUAHAHAHA

--- beta/www/js/app.js
+++ beta/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
@@ -15,20 +15,24 @@ angular.module('starter', ['ionic'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('navigator', {
+    $stateProvider
+
+    // the default route provider for the things that will never change on the page
+    .state('navigator', {
+        url: '',
         abstract: true,
-        templateUrl: 'templates/shell.html'
-    }).state('navigator.home', {
+        templateUrl: 'templates/shell.html',
+        controller: 'ViewController'
+    })
+    // route for the home page
+    .state('navigator.home', {
         url: '/home',
         views: {
             'home': {
-                templateUrl: 'templates/home.html'
+                templateUrl: 'templates/home.html',
+                controller: 'ViewController'
             }
         }
     })
     $urlRouterProvider.otherwise('/home')
-})
-
-.controller('ViewController', ['$scope', '$http'], function($scope, $http) {
-
 })
