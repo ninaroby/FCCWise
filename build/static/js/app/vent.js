eaabfee1030028675db98146d5737df1abb7be01
Nina Roby <ninaroby.usa@gmail.com>
2015-07-08 15:26:22 -0700

    removed the build repository

--- build/static/js/app/vent.js
+++
@@ -1,7 +0,0 @@
-define(function (require, exports, module) {
-    var Backbone = require('backbone')
-    var Marionette = require('marionette')
-
-    var vent = new Backbone.Wreqr.EventAggragator()
-    exports.vent = vent
-})
