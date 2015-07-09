8fd3d0bd143bcd81fb92b40864f76c2edf548ce5
juliancoleman <julcol03@gmail.com>
2015-07-09 15:13:23 -0700

    just about done with the home page

--- beta/www/js/controllers.js
+++ beta/www/js/controllers.js
@@ -1,33 +1,32 @@
 angular.module('starter.controllers', [])
 
-.controller('ViewController', function($scope, $ionicModal, $ionicHistory, $sce) {
-    $scope.reportData = {}
+.controller('ViewController', function($scope, $http, $ionicModal, $ionicHistory, $sce) {
+    // grab the news and updates file
+    $http.get('js/news.json').success(function(data) {
+        $scope.news = data
+    })
+
+    // url safe linking
     $scope.trustSrc = function(src) {
         return $sce.trustAsResourceUrl(src);
     }
 
-    // modal view for the suggestion link
+    // suggestion box modal
+    $scope.reportData = {}
     $ionicModal.fromTemplateUrl('templates/suggestions.html', {
         scope: $scope
     }).then(function(modal) {
         $scope.modal = modal
     })
-
     $scope.closeModal = function() {
         $scope.modal.hide()
     }
-
     $scope.showModal = function() {
         $scope.modal.show()
         $scope.reportData.currentPage = $ionicHistory.currentTitle()
     }
-
     $scope.doSuggestion = function() {
         console.log('Processing Suggestion', $scope.reportData)
-
-        // send report data below
-
-        // close when done
         $scope.closeModal()
         document.querySelector('input').value = ''
     }
