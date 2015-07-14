ef60f7662cc2afc81fd061db21d2f19aaef0d159
juliancoleman <julcol03@gmail.com>
2015-07-14 14:59:39 -0700

    merge

--- beta/www/js/controllers.js
+++ beta/www/js/controllers.js
@@ -2,23 +2,26 @@ angular.module('starter.controllers', ['ngAnimate'])
 
 .controller('ViewController', function($scope, $http, $ionicModal, $ionicHistory, $sce, GetData, FIREBASE_URL) {
     // get the tutor datastore from Firebase
-    $scope.tutors = GetData
+    // $scope.tutors = GetData
+    // $scope.tutor = {}
     // add tutors to Firebase
-    $scope.addTutor = function() {
-        GetData.$add($scope.tutor)
-    }
+    // $scope.addTutor = function() {
+    //     GetData.$add($scope.tutor)
+    // }
     // remove tutors from Firebase
-    $scope.removeTutor = function(deleteID) {
-        console.log(deleteID)
-        var deleteRef = new Firebase(FIREBASE_URL + deleteID)
-        deleteRef.remove()
-    }
+    // $scope.removeTutor = function(deleteID) {
+    //     console.log(deleteID)
+    //     var deleteRef = new Firebase(FIREBASE_URL + deleteID)
+    //     deleteRef.remove()
+    // }
 
     // get the links and stuff
     $http.get('js/store.js').success(function(data) {
-        $scope.links = data.sitemap
         $scope.news = data.news
         $scope.calendar = data.calendar
+    })
+    $http.get('js/routes.js').success(function(data) {
+        $scope.links = data
     })
 
     // url safe linking
--- beta/www/js/tutors.js
+++ beta/www/js/tutors.js
@@ -3,8 +3,9 @@
         "firstName": "Julian",
         "lastName": "Coleman",
         "thumbnail": "https://avatars1.githubusercontent.com/u/11545939?v=3&s=460",
-        "subjects": ["Computer Science", "Web Design"],
+        "subjects": ["Computer Science"],
         "courses": ["CSCI-40", "CSCI-41", "CIT-62", "CIT-85"],
+        "hiring_department": "70617373",
         "etc": false,
         "schedule": [
             {
@@ -60,6 +61,7 @@
         "thumbnail": "https://avatars1.githubusercontent.com/u/8042343?v=3&s=460",
         "subjects": ["Computer Information Systems", "Web Design"],
         "courses": ["CIT-62", "CIT-15"],
+        "hiring_department": "70617373",
         "etc": false,
         "schedule": [
             {
@@ -115,6 +117,7 @@
         "thumbnail": "http://fccwise.fresnocitycollege.edu/tutorialcenter/searchtutor/images/mileena.jpg",
         "subjects": ["Algebra", "Trigonometry", "Precalculus", "Calculus"],
         "courses": ["Math-103", "Math-260 (A, B, C, D)", "Math-4A", "Math-4B", "Math-5A", "Math-5B"],
+        "hiring_department": "7463",
         "etc": false,
         "schedule": [
             {
@@ -158,6 +161,147 @@
                 "details": [
                     {
                         "times": "9:00 AM - 2:00 PM",
+                        "location": "Tutorial Center"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "firstName": "Adrian",
+        "lastName": "Ramos",
+        "thumbnail": "http://fccwise.fresnocitycollege.edu/tutorialcenter/searchtutor/images/adrain%203.jpg",
+        "subjects": ["Intermediate Algebra", "Pre-Algebra", "Arithmetic Review", "Precalculus", "Calculus"],
+        "courses": ["Math-103", "Math-255", "Math-260 (A, B, C, D)", "Math-5A", "Math-5B"],
+        "hiring_department": "7463",
+        "etc": false,
+        "schedule": [
+            {
+                "weekday": "Monday",
+                "details": [
+                    {
+                        "times": "1:00 PM - 4:30 PM",
+                        "location": "Tutorial Center"
+                    }
+                ]
+            },
+            {
+                "weekday": "Tuesday",
+                "details": [
+                    {
+                        "times": "1:00 PM - 4:30 PM",
+                        "location": "Tutorial Center"
+                    }
+                ]
+            },
+            {
+                "weekday": "Wednesday",
+                "details": [
+                    {
+                        "times": "1:00 PM - 4:30 PM",
+                        "location": "Tutorial Center"
+                    }
+                ]
+            },
+            {
+                "weekday": "Thursday",
+                "details": [
+                    {
+                        "times": "1:00 PM - 4:30 PM",
+                        "location": "Tutorial Center"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "firstName": "Brent",
+        "lastName": "Stephens",
+        "thumbnail": "http://fccwise.fresnocitycollege.edu/tutorialcenter/searchtutor/images/Brent.jpg",
+        "subjects": ["Pre-Algebra", "Elementary Algebra", "Arithmetic Review", "Trigonometry", "Precalculus"],
+        "courses": ["Math-103", "Math-201", "Math-260 (A, B, C, D)", "Math-4A", "Math-4B"],
+        "hiring_department": "7463",
+        "etc": false,
+        "schedule": [
+            {
+                "weekday": "Monday",
+                "details": [
+                    {
+                        "times": "8:00 AM - 10:00 AM",
+                        "location": "Tutorial Center"
+                    }
+                ]
+            },
+            {
+                "weekday": "Tuesday",
+                "details": [
+                    {
+                        "times": "8:00 AM - 10:00 AM",
+                        "location": "Tutorial Center"
+                    }
+                ]
+            },
+            {
+                "weekday": "Wednesday",
+                "details": [
+                    {
+                        "times": "8:00 AM - 10:00 AM",
+                        "location": "Tutorial Center"
+                    }
+                ]
+            },
+            {
+                "weekday": "Thursday",
+                "details": [
+                    {
+                        "times": "7:30 AM - 10:00 AM",
+                        "location": "Tutorial Center"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "firstName": "Diandra",
+        "lastName": "Sam",
+        "thumbnail": "http://fccwise.fresnocitycollege.edu/tutorialcenter/searchtutor/images/Diandra956.jpg",
+        "subjects": ["Pre-Algebra", "Elementary Algebra", "Arithmetic Review", "Trigonometry", "Precalculus"],
+        "courses": ["Math-103", "Math-201", "Math-260 (A, B, C, D)", "Math-4A", "Math-4B"],
+        "hiring_department": "7463",
+        "etc": false,
+        "schedule": [
+            {
+                "weekday": "Monday",
+                "details": [
+                    {
+                        "times": "8:00 AM - 10:00 AM",
+                        "location": "Tutorial Center"
+                    }
+                ]
+            },
+            {
+                "weekday": "Tuesday",
+                "details": [
+                    {
+                        "times": "8:00 AM - 10:00 AM",
+                        "location": "Tutorial Center"
+                    }
+                ]
+            },
+            {
+                "weekday": "Wednesday",
+                "details": [
+                    {
+                        "times": "8:00 AM - 10:00 AM",
+                        "location": "Tutorial Center"
+                    }
+                ]
+            },
+            {
+                "weekday": "Thursday",
+                "details": [
+                    {
+                        "times": "7:30 AM - 10:00 AM",
                         "location": "Tutorial Center"
                     }
                 ]
