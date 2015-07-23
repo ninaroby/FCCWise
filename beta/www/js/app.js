f701e5387e02bc27f055f353a2434af6cad8e81d
juliancoleman <julcol03@gmail.com>
2015-07-23 08:33:44 -0700

    Concatenated JS files and reduces network requests

--- beta/www/js/app.js
+++ beta/www/js/app.js
@@ -1,4 +1,4 @@
-angular.module('starter', ['ionic', 'starter.controllers', 'firebase'])
+angular.module('starter', ['ionic', 'firebase'])
 .constant('FIREBASE_URL', 'https://fccwise-search.firebaseio.com/')
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
@@ -141,6 +141,205 @@ angular.module('starter', ['ionic', 'starter.controllers', 'firebase'])
         }
     });
     $urlRouterProvider.otherwise('/home');
+})
+.controller('ViewController', function($scope, $http, $ionicModal, $ionicHistory, $sce, GetData, FIREBASE_URL) {
+    // get the tutor datastore from Firebase
+    $scope.tutors = GetData;
+
+    // we want to pre-format the schedule so that when we push it to the tutor object and submit it to
+    // Firebase, it'll be in the correct format to display on the page and we won't have to rewrite
+    // the logic in `engine.html` for all the ng-repeats we have in that beast.
+    $scope.tutor = {
+        schedule: [
+            {
+                weekday: "Monday",
+                details: []
+            },
+            {
+                weekday: "Tuesday",
+                details: []
+            },
+            {
+                weekday: "Wednesday",
+                details: []
+            },
+            {
+                weekday: "Thursday",
+                details: []
+            },
+            {
+                weekday: "Friday",
+                details: []
+            },
+            {
+                weekday: "Saturday",
+                details: []
+            }
+        ]
+    };
+
+    // If, by chance, a tutor works 2 or more shifts in a single day, we want to make an add button that
+    // appends a pre-formatted object to the `details` array. This information causes a single ng-repeat
+    // for each time the add button is clicked. In fact, if a tutor doesn't work one day, we don't even
+    // want there to be a form available for that day. You must click "Add" at least once in order to
+    // say a tutor works today, otherwise the day is skipped and the tutor does not have a schedule for
+    // that day
+    $scope.addDetails = function(e, schedule) {
+        e.preventDefault();
+        schedule.details.push({location:"Tutorial Center"});
+    };
+
+    // adding tutors to Firebase
+    $scope.addTutor = function() {
+        // format everything exactly how I want them to be formatted. There is no room for errors.
+
+        // we'll generate the thumbnail links for each tutor
+        $scope.tutor.thumbnail = 'img/tutors/' + $scope.tutor.firstName.toLowerCase() + '_' + $scope.tutor.lastName.toLowerCase() + '.jpg';
+
+        // check to see if the form is filled. If it is, take each item, split at the comma (if there is at
+        // least one), and then iterate over each item to remove excess whitespace along the left and right
+        // side of the query. This cannot trim the actual text for the reason that it looks at the entire
+        // string and trims the far left and far right. To get around this, simply split, and then trim each
+        // item in the array.
+        if ($scope.tutor.short.value !== '') {
+            $scope.tutor.short = document.querySelector('#classes').value.split(',');
+            for (var i in $scope.tutor.short) {
+                $scope.tutor.short[i] = $scope.tutor.short[i].trim();
+            }
+        }
+
+        // again, check to see if the form has been filled. If it is, split at the comma (if there is one),
+        // trim the whitespace from each item in the array, and then push the array.
+        if ($scope.tutor.subjects.value !== '') {
+            $scope.tutor.subjects = document.querySelector('#subjects').value.split(',');
+            for (var j in $scope.tutor.subjects) {
+                $scope.tutor.subjects[j] = $scope.tutor.subjects[j].trim();
+            }
+        }
+
+        // again, check to see if the form has been filled. If it is, split at the comma (if there is one),
+        // trim the whitespace from each item in the array, and then push the array.
+        if ($scope.tutor.courses.value !== '') {
+            $scope.tutor.courses = document.querySelector('#courses').value.split(',');
+            for (var k in $scope.tutor.courses) {
+                $scope.tutor.courses[k] = $scope.tutor.courses[k].trim().split(' ').join('-').toUpperCase();
+            }
+        }
+
+        // in checking against etc, the current search implementation
+        // looks at the left side of the colon for some reason. If the
+        // tutor is not etc, simply don't even apply the .etc property.
+        // That way Firebase won't index it. There is only one test case.
+        if ($scope.tutor.etc.toUpperCase() === 'YES') {
+            $scope.tutor.etc = true;
+        } else {
+            $scope.tutor.etc = null;
+        }
+        if ($scope.tutor.schedule.instructor === '') {
+            $scope.tutor.schedule.instructor = null;
+        }
+
+        // remove the days that are empty
+        $scope.tutor.schedule = _.filter($scope.tutor.schedule, function(schedule) {
+            return schedule.details.length !== 0;
+        });
+
+        // Finally, we'll push the tutor data to Firebase.
+        GetData.$add($scope.tutor);
+
+        // And then clear out the form for the next person
+        // document.querySelector('form').reset()
+        window.location.reload(forceGet);
+    };
+
+    // remove tutors from Firebase
+    $scope.removeTutor = function(deleteID) {
+        console.log(deleteID);
+        var deleteRef = new Firebase(FIREBASE_URL + deleteID);
+        deleteRef.remove();
+    };
+
+    // search by multiple queries
+    $scope.searchFunc = function(item) {
+        var invalidSearchTerms = [' and ', ' or ', 'courses', 'firstName', 'hiring_department', 'lastName', 'schedule', 'details', 'weekday', 'location', 'times', 'short', 'subjects', 'thumbnail'];
+        var jsonStr = angular.lowercase(JSON.stringify(item));
+        if ($scope.queryStr && $scope.queryStr.trim()) {
+            var query = $scope.queryStr.trim().split(' ');
+            var result = true;
+            for (var partIndex in query) {
+                if (jsonStr.indexOf(angular.lowercase(query[partIndex])) > -1 && _.contains(invalidSearchTerms, query[partIndex]) !== true) {
+                    result = result && true;
+                } else {
+                    result = false;
+                }
+            }
+            return result;
+        } else {
+            return true;
+        }
+    };
+
+    // get the links and stuff
+    $http.get('js/store.js').success(function(data) {
+        $scope.news = data.news;
+        $scope.links = data.routes;
+    });
+
+    // url safe linking
+    $scope.trustSrc = function(src) {
+        return $sce.trustAsResourceUrl(src);
+    };
+
+    // suggestion box modal
+    $scope.reportData = {};
+    $ionicModal.fromTemplateUrl('templates/suggestions.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
+    $scope.showModal = function() {
+        $scope.modal.show();
+        $scope.reportData.currentPage = $ionicHistory.currentTitle();
+    };
+    $scope.doSuggestion = function() {
+        console.log('Processing Suggestion', $scope.reportData);
+        $scope.closeModal();
+        document.querySelector('input').value = '';
+    };
+
+    // calendar
+    $scope.calendar = {
+        today: new Date(),
+        months: [
+            {"name": "January", "days": 31},
+            {"name": "February", "days": null},
+            {"name": "March", "days": 31},
+            {"name": "April", "days": 30},
+            {"name": "May", "days": 31},
+            {"name": "June", "days": 30},
+            {"name": "July", "days": 31},
+            {"name": "August", "days": 31},
+            {"name": "September", "days": 30},
+            {"name": "October", "days": 31},
+            {"name": "November", "days": 30},
+            {"name": "December", "days": 31}
+        ],
+        getCurrentMonth: function() {
+            return this.months[this.today.getMonth()].name;
+        },
+        tick: function tick() {
+            var today = new Date();
+            this.today = today;
+            var intervalId = setTimeout(tick, 900000);
+            console.log('Timer Updated');
+        }
+    };
+    // use moment.js for enforcing of dates
+    // generating dates in workshop
+    $scope.calendar.tick();
 })
 .factory('GetData', function($firebaseArray, FIREBASE_URL) {
     var ref = new Firebase(FIREBASE_URL);
--- beta/www/js/store.js
+++ beta/www/js/store.js
@@ -15,5 +15,61 @@
             "synopsis": "The tutor of the Year Award goes to the awesome Vinh Le.  Hear what he has to say about his love of helping students and how his time with the tutorial center will be part of his \"unforgettable memories\".",
             "id": "best_tutor"
         }
+    ],
+    "routes": [
+        {
+            "path": "#/search",
+            "thumbnail": "img/tutor_search.png",
+            "alt": "Tutor Search",
+            "title": "Find a Tutor"
+        },
+        {
+            "path": "#/extending-the-class",
+            "thumbnail": "img/etc.png",
+            "alt": "Extending the Class",
+            "title": "Extending the Class"
+        },
+        {
+            "path": "#/tutorial-center",
+            "thumbnail": "img/tc.png",
+            "alt": "Tutorial Center",
+            "title": "Tutorial Center"
+        },
+        {
+            "path": "#/writing-reading-center",
+            "thumbnail": "img/wrc.png",
+            "alt": "Writing & Reading Center",
+            "title": "Writing & Reading Center"
+        },
+        {
+            "path": "#/pass",
+            "thumbnail": "img/pass.png",
+            "alt": "PASS",
+            "title": "PASS"
+        },
+        {
+            "path": "#/become-a-tutor",
+            "thumbnail": "img/b_tutor.png",
+            "alt": "Become a Tutor",
+            "title": "Become a Tutor"
+        },
+        {
+            "path": "#/calendar",
+            "thumbnail": "img/workshops.png",
+            "alt": "Workshop Calendar",
+            "title": "Workshop Calendar"
+        },
+        {
+            "path": "https://scccd.blackboard.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_60_1",
+            "thumbnail": "img/online.png",
+            "alt": "Online Tutoring",
+            "title": "Online Tutoring"
+        },
+        {
+            "path": "#/resources",
+            "thumbnail": "img/ed_resources.png",
+            "alt": "Eductational Resources",
+            "title": "Eductational Resources"
+        }
     ]
 }
